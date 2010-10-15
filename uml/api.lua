2e0fff66b5967cdf4f73dfb71c124da6c735f444
Francois Perrad <francois.perrad@gadz.org>
2010-10-15 17:49:28 +0200

    s/format/formats/ it is an array

--- uml/api.lua
+++ uml/api.lua
@@ -12,7 +12,7 @@ has.optional_params = { is = 'ro', isa = 'table<string>' }
 has.base_url        = { is = 'ro', isa = 'string' }
 has.description     = { is = 'ro', isa = 'string' }
 has.authentication  = { is = 'ro', isa = 'boolean' }
-has.format          = { is = 'ro', isa = 'table<string>' }
+has.formats         = { is = 'ro', isa = 'table<string>' }
 
 
 class 'API'
@@ -22,7 +22,7 @@ has.methods         = { is = 'ro', isa = 'table<string,Method>', required = true
 has.base_url        = { is = 'ro', isa = 'string' }
 has.description     = { is = 'ro', isa = 'string' }
 has.authentication  = { is = 'ro', isa = 'boolean' }
-has.format          = { is = 'ro', isa = 'table<string>' }
+has.formats         = { is = 'ro', isa = 'table<string>' }
 has.version         = { is = 'ro', isa = 'string' }
 has.authority       = { is = 'ro', isa = 'string' }
 has.meta            = { is = 'ro', isa = 'table<string,string>' }
