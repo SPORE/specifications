d26bbba5c1a46162695b950ed045f0c3057f8077
Francois Perrad <francois.perrad@gadz.org>
2010-10-15 16:36:05 +0200

    more explicit names

--- uml/api.lua
+++ uml/api.lua
@@ -6,9 +6,9 @@ class 'Method'
 
 has._method         = { is = 'ro', isa = 'string', required = true }
 has.path            = { is = 'ro', isa = 'string', required = true }
-has.expected        = { is = 'ro', isa = 'table<number>' }
-has.required        = { is = 'ro', isa = 'table<string>' }
-has.optional        = { is = 'ro', isa = 'table<string>' }
+has.expected_status = { is = 'ro', isa = 'table<number>' }
+has.required_params = { is = 'ro', isa = 'table<string>' }
+has.optional_params = { is = 'ro', isa = 'table<string>' }
 has.base_url        = { is = 'ro', isa = 'string' }
 has.description     = { is = 'ro', isa = 'string' }
 has.authentication  = { is = 'ro', isa = 'boolean' }
@@ -22,8 +22,8 @@ has.methods         = { is = 'ro', isa = 'table<string,Method>', required = true
 has.base_url        = { is = 'ro', isa = 'string' }
 has.description     = { is = 'ro', isa = 'string' }
 has.authentication  = { is = 'ro', isa = 'boolean' }
+has.format          = { is = 'ro', isa = 'table<string>' }
 has.version         = { is = 'ro', isa = 'string' }
 has.authority       = { is = 'ro', isa = 'string' }
-has.format          = { is = 'ro', isa = 'table<string>' }
 has.meta            = { is = 'ro', isa = 'table<string,string>' }
 
